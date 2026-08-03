c48f7ec9d234027f0b779098ac8ffdfb500807b7
Haykashen <Haykashen@yandex.rugit config --global user.email Haykashen@yandex.ru>
2026-08-03 17:05:16 +0300

    onboarding review

--- src/app/(onboarding)/onboarding.tsx
+++ src/app/(onboarding)/onboarding.tsx
@@ -1,3 +1,4 @@
+import PaginationItem from '@/components/items/PaginationItem';
 import { Context } from '@/context/context';
 import { setData } from '@/store/setData';
 import { scaleEnd, scaleStart } from '@/utils/animation';
@@ -8,25 +9,25 @@ import { useContext, useRef, useState } from 'react';
 import { Animated, ImageBackground, Pressable, StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-const OnboardingScreen = ({ path, title, text }: { path: string, title: string, text: string,  }) => {
+const OnboardingScreen = ({ path, title, text }: { path: string, title: string, text: string, }) => {
 
-    return (
-        <View style={{ height: '100%', flexDirection: 'column' }}>
-            <LottieView style={{ flex: 1 }} source={path} autoPlay loop />
-            <View style={{ flexDirection: 'column', alignItems: 'center', padding: 20 }}>
-                <Text numberOfLines={2} style={{ color: 'white', fontSize: 28, fontWeight: 'bold', justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}>{title}</Text>
-                <Text numberOfLines={3} style={{ color: 'white', fontSize: 20, justifyContent: 'center', alignContent: 'center', textAlign: 'center'}}>{text}</Text>
-            </View>
-        </View>
-    )
+  return (
+    <View style={{ height: '100%', flexDirection: 'column' }}>
+      <LottieView style={{ flex: 1 }} source={path} autoPlay loop />
+      <View style={{ flexDirection: 'column', alignItems: 'center', padding: 20 }}>
+        <Text numberOfLines={2} style={{ color: 'white', fontSize: 28, fontWeight: 'bold', justifyContent: 'center', alignContent: 'center', textAlign: 'center' }}>{title}</Text>
+        <Text numberOfLines={3} style={{ color: 'white', fontSize: 20, justifyContent: 'center', alignContent: 'center', textAlign: 'justify', marginHorizontal: 5 }}>{text}</Text>
+      </View>
+    </View>
+  )
 }
 
 const onboarding = () => {
-    const [page, setPage] = useState(0)
-    const pagerRef = useRef<PagerViewRef>(null);
-    const { setOnboarded } = useContext(Context);
+  const [page, setPage] = useState(0)
+  const pagerRef = useRef<PagerViewRef>(null);
+  const { setOnboarded } = useContext(Context);
   const scale = useRef(new Animated.Value(1)).current;
-    const screen = require('@/assets/animation/Business_plan.json');
+  const screen = require('@/assets/animation/Business_plan.json');
 
   // Функция для анимации нажатия
   const handlePressIn = () => {
@@ -37,64 +38,64 @@ const onboarding = () => {
   const handlePressOut = () => {
     scaleEnd(scale, 1)
   };
-    const OnboardingDone = ()=>{
-      setData('onboarded', JSON.stringify(true))
-      setOnboarded(true)
-      router.push('/')
-    }
-    return (
-        <SafeAreaView style={{ flex: 1, backgroundColor: '#031F2B', paddingTop: 5, flexDirection: 'column', gap: 25, justifyContent:'center'}}>
-            <View style={{ height:'70%' }}>
-                <PagerView
-                    ref={pagerRef}
-                    style={{ flex: 1 }}
-                    initialPage={0}
-                    onPageSelected={event => {
-                        setPage(event.nativeEvent.position)
-                        console.log('selected page', event.nativeEvent.position);
-                    }}>
-                    <View style={{ height: '100%', flexDirection: 'column', justifyContent: 'center' }}>
-                        <Animated.View style={{ transform: [{ scale }] }}>
-                            <ImageBackground source={require('@/assets/images/icon.png')} style={{ width: 'auto', height: '91%', justifyContent: 'flex-end', gap: 10 }}>
-                                <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold', justifyContent: 'center', alignContent: 'center', textAlign: 'center', marginHorizontal:5 }}>{'Добро пожаловать в Хелдон!'}</Text>
-                                <Text numberOfLines={3} style={{ color: 'white', fontSize: 20, justifyContent: 'center', alignContent: 'center', textAlign: 'justify', marginHorizontal:5  }}>{'Хелдон - ваш личный помощник для эффективного планирования времени'}</Text>
+  const OnboardingDone = () => {
+    setData('onboarded', JSON.stringify(true))
+    setOnboarded(true)
+    router.push('/')
+  }
+  return (
+    <SafeAreaView style={{ flex: 1, backgroundColor: '#031F2B', paddingTop: 5, flexDirection: 'column', gap: 25, justifyContent: 'center' }}>
+      <View style={{ height: '70%' }}>
+        <PagerView
+          ref={pagerRef}
+          style={{ flex: 1 }}
+          initialPage={0}
+          onPageSelected={event => {
+            setPage(event.nativeEvent.position)
+            console.log('selected page', event.nativeEvent.position);
+          }}>
+          <View style={{ height: '100%', flexDirection: 'column', justifyContent: 'center' }}>
+            <Animated.View style={{ transform: [{ scale }] }}>
+              <ImageBackground source={require('@/assets/images/icon.png')} style={{ width: 'auto', height: '91%', justifyContent: 'flex-end', gap: 10 }}>
+                <Text style={{ color: 'white', fontSize: 28, fontWeight: 'bold', justifyContent: 'center', alignContent: 'center', textAlign: 'center', marginHorizontal: 5 }}>{'Добро пожаловать в Хелдон!'}</Text>
+                <Text numberOfLines={3} style={{ color: 'white', fontSize: 20, justifyContent: 'center', alignContent: 'center', textAlign: 'justify', marginHorizontal: 5 }}>{'Хелдон - ваш личный помощник для эффективного планирования времени'}</Text>
 
-                            </ImageBackground>
-                            {/* <Image source={require('@/assets/images/icon.png')} style={{width:'100%',}} /> */}
-                        </Animated.View>
-                    </View>
+              </ImageBackground>
+              {/* <Image source={require('@/assets/images/icon.png')} style={{width:'100%',}} /> */}
+            </Animated.View>
+          </View>
 
-                    <OnboardingScreen
-                      path={screen} 
-                      title='Наведите порядок в хаосе дел и планов' 
-                      text='Создавайте задачи и распределяйте их на необходимые даты, чтобы избежать суеты'
-                    />
-                    <OnboardingScreen 
-                      path={require('@/assets/animation/Growth_Assistance.json')} 
-                      title=""
-                      text='Создавайте задачи и распределяйте их на необходимые даты, чтобы избежать суеты'
-                    />
-                    <OnboardingScreen 
+          <OnboardingScreen
+            path={screen}
+            title='Наведите порядок в хаосе дел и планов'
+            text='Создавайте задачи и равномерно распределяйте их по датам, чтобы избежать суеты'
+          />
+          <OnboardingScreen
+            path={require('@/assets/animation/Growth_Assistance.json')}
+            title="Повышайте эффективность"
+            text='Ежедневный контроль задач - гарантия результативного проведения дня'
+          />
+          {/* <OnboardingScreen 
                       path={require('@/assets/animation/Holding_value.json')} 
                       title='Выполняйте ежедневный план дел' 
                       text='Ежедневно контролируйте выполнение поставленных задач, что ваши дни были плодотворными'
-                    />
-                    <OnboardingScreen 
-                      path={require('@/assets/animation/Rotate_a_chart.json')} 
-                      title='Welcome!!!'
-                      text=""
-                    />
-                    <OnboardingScreen 
-                      path={require('@/assets/animation/Successful_target.json')} 
-                      title='Welcome!!!'
-                      text="" 
-                    />
-                    <OnboardingScreen 
+                    /> */}
+          <OnboardingScreen
+            path={require('@/assets/animation/Rotate_a_chart.json')}
+            title='Анализируйте план задач'
+            text="Анализ плана задач поможет равномерно распределять усилия по всем сферам жизни"
+          />
+          <OnboardingScreen
+            path={require('@/assets/animation/Successful_target.json')}
+            title='Достигайте целей'
+            text="Выполняйте поставленные задачи, плодотворно проводя время и достигая своих целей"
+          />
+          {/* <OnboardingScreen 
                       path={require('@/assets/animation/Task_Loader.json')} 
-                      title='Welcome!!!' 
-                      text=""
-                    />
-                    <OnboardingScreen 
+                      title='Достигайте свои цели' 
+                      text="Выполняйте поставленные задачи, плодотворно проводя время и достигая своих целей"
+                    /> */}
+          {/* <OnboardingScreen 
                       path={require('@/assets/animation/The_idea_of_change.json')} 
                       title='Welcome!!!'
                       text="" 
@@ -103,30 +104,40 @@ const onboarding = () => {
                       path={require('@/assets/animation/Web_Design.json')} 
                       title='Welcome!!!'
                       text=""
-                    />
-                </PagerView>
-            </View>
-            <View style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
-                <Pressable onPress={() => pagerRef.current?.setPage(page === 7 ? 0 : page + 1)}>
-                    <Text style={{ color: 'white', fontSize: 22, fontWeight: 'bold' }}>Go to page {page + 1}</Text>
-                </Pressable>
-                {page === 7 && <Pressable onPress={OnboardingDone}>
-                    <Text style={{ color: 'white', fontSize: 22, fontWeight: 'bold' }}>Done</Text>
-                </Pressable>}                
-            </View>
-        </SafeAreaView>
-    )
+                    /> */}
+        </PagerView>
+      </View>
+      <View style={{ flexDirection: 'column', alignItems: 'center', gap:10}}>
+        <View style={{ width: '50%', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
+          <PaginationItem onPress={() => pagerRef.current?.setPage(0)} value={0} currentValue={page} />
+          <PaginationItem onPress={() => pagerRef.current?.setPage(1)} value={1} currentValue={page} />
+          <PaginationItem onPress={() => pagerRef.current?.setPage(2)} value={2} currentValue={page} />
+          <PaginationItem onPress={() => pagerRef.current?.setPage(3)} value={3} currentValue={page} />
+          <PaginationItem onPress={() => pagerRef.current?.setPage(4)} value={4} currentValue={page} />
+        </View>
+        {page !== 4 && <View
+          style={{ padding: 5, borderRadius: 5, width: '50%', margin: 'auto', justifyContent:'center', alignItems:'center' }}>
+          <Text style={{ color: '#031F2B', fontWeight: 'bold', fontSize: 20 }}>Готово</Text>
+        </View> }
+        {page === 4 && <Pressable
+          onPress={OnboardingDone}
+          style={{ backgroundColor: '#007aff', padding: 5, borderRadius: 5, width: '50%', margin: 'auto', justifyContent:'center', alignItems:'center' }}>
+          <Text style={{ color: 'white', fontWeight: 'bold', fontSize: 20 }}>Готово</Text>
+        </Pressable>}
+      </View>
+    </SafeAreaView>
+  )
 }
 
 export default onboarding
 
 const styles = StyleSheet.create({
-    setting_row: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-    },
-    page: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+  setting_row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  page: { flex: 1, alignItems: 'center', justifyContent: 'center' },
 
 
 });
---
+++ src/components/items/PaginationItem.tsx
@@ -0,0 +1,36 @@
+import { scaleEnd, scaleStart } from '@/utils/animation';
+import { useRef } from 'react';
+import { Animated, Pressable } from 'react-native';
+
+export type TPaginationItem= { onPress :(arg:number)=>void, value:number, currentValue:number }
+
+const PaginationItem = ({ onPress, value, currentValue }: TPaginationItem) => {
+
+    const scale = useRef(new Animated.Value(1)).current;
+
+    // Функция для анимации нажатия
+    const handlePressIn = () => {
+        scaleStart(scale, 1.5)
+    };
+
+    // Возврат к обычному размеру
+    const handlePressOut = () => {
+        scaleEnd(scale, 1)
+    };
+
+    const handlePress = () => {
+        onPress(value)//id
+    }
+
+    return (
+        <Animated.View style={{ transform: [{ scale }] }}>
+            <Pressable
+                onPress={handlePress}
+                onPressIn={handlePressIn}
+                onPressOut={handlePressOut}
+                style={{ height: 4, width: 20, backgroundColor: value === currentValue ? '#007aff' : 'white' }} />
+        </Animated.View>
+    )
+}
+
+export default PaginationItem
